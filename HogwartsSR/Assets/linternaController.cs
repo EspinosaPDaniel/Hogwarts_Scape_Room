8703bfd32aef7eea952383041336756b0cc2cca4
Yissuss16 <jess.ortega3777@gmail.com>
2023-05-02 17:24:56 +0200

    Avance

--- HogwartsSR/Assets/linternaController.cs
+++ HogwartsSR/Assets/linternaController.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class linternaController : MonoBehaviour
 {
     public Light light;
     public Light light2;
+	private SteamVR_Input_Sources inputSource;
+	private SteamVR_Action_Boolean buttonAction;
     // Start is called before the first frame update
    void Start()
     {
@@ -13,11 +16,16 @@ public class linternaController : MonoBehaviour
         light.enabled = false;
         light2.enabled = false;
     }
+	void Awake()
+    {
+        inputSource = SteamVR_Input_Sources.RightHand;
+	   buttonAction = SteamVR_Actions.default_SnapTurnRight;
+    }
     
     void Update()
     {
         // Si se presiona la tecla F, cambiamos el estado de la linterna
-        if (Input.GetKeyDown(KeyCode.F))
+        if(buttonAction.GetLastStateDown(inputSource))
         {
             light.enabled = !light.enabled;
             light2.enabled = !light2.enabled;
