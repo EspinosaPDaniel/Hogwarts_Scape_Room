8bba14581bbabcbe7bb08fc43fd3b21549efc16a
ahonrubia <ahonrubia@live.com>
2023-05-31 18:54:50 +0200

    cambios

---
+++ HogwartsSR/Assets/Teleport2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport2 : MonoBehaviour
+{
+  public Transform teleportTarget; // La posición a la que deseas teletransportarte
+  public int prueba=0;
+
+
+    public GameObject jugador;
+
+    private void OnColliderEnter(Collider other)
+    {
+
+        prueba=1;
+        // Comprueba si el objeto que atraviesa la zona de teletransporte es el jugador
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.name);
+            prueba=10;
+            Debug.Log("Prueba cambiado");
+            // Teletransporta al jugador a la nueva posición
+
+           
+            jugador.transform.position = teleportTarget.position;
+            //other.transform.position = teleportTarget.position;
+            //other.GetComponent<Transform>().position = teleportTarget.position;
+            Debug.Log("Se ha producidxzgfvfdgdsgo el transporte");
+        }
+    }
+
+
+}
+
