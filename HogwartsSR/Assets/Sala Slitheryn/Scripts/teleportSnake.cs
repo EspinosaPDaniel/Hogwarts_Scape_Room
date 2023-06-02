fea0b82468786a788218470f1108fee29d34185d
Yissuss16 <jess.ortega3777@gmail.com>
2023-06-02 10:58:14 +0200

    cambios

--- HogwartsSR/Assets/Sala Slitheryn/Scripts/teleportSnake.cs
+++ HogwartsSR/Assets/Sala Slitheryn/Scripts/teleportSnake.cs
@@ -6,9 +6,12 @@ public class Snaketeleport : MonoBehaviour
 {
     public Transform[] teleportLocations; // Array con las tres ubicaciones de teletransportación
     private int currentIndex = 0; // Índice actual de la ubicación de teletransportación
-    public float teleportDelay = 25f; // Tiempo en segundos entre cada teletransportación
+    public float teleportDelay = 15f; // Tiempo en segundos entre cada teletransportación
 
     private float timer = 0f; // Temporizador para realizar el teletransporte
+
+    public int contadorColisiones = 0; //Inicializacion de las colisiones
+    public int colisionesNecesarias = 3; //Colisiones necesarias
 
     private void Update()
     {
@@ -27,6 +30,23 @@ public class Snaketeleport : MonoBehaviour
         // Teletransportar el objeto a la ubicación actual y luego avanzar al siguiente índice
         transform.position = teleportLocations[currentIndex].position;
         currentIndex = (currentIndex + 1) % teleportLocations.Length;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Comprobar si la colisión es con la esfera
+        if (collision.gameObject.CompareTag("Sphere"))
+        {
+            contadorColisiones++;
+
+            // Comprobar si se alcanzó el número necesario de colisiones
+            if (contadorColisiones >= colisionesNecesarias)
+            {
+                // Desaparecer el objeto
+                Destroy(gameObject);
+            }
+        }
     }
 }
 
